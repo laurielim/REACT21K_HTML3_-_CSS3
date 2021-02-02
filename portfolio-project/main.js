a9ea63abecc902af7fdceb7812a4192f6eb4b08b
laurielim <laurie.limsam@gmail.com>
2021-02-02 15:26:44 +0200

    change header on scroll

---
+++ portfolio-project/main.js
@@ -0,0 +1,30 @@
+let header = document.querySelector("#header");
+let logo = document.querySelector("#logo");
+
+const minHeader = () => {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    logo.style.fontSize = "32px";
+    header.style.backgroundColor = "#05080fee";
+  } else {
+    logo.style.fontSize = "60px";
+    header.style.backgroundColor = "#05080f00";
+  }
+};
+document.addEventListener("scroll", minHeader);
+
+let backToTop = document.querySelector(".back-to-top");
+
+const toTop = () => {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    backToTop.style.bottom = "20px";
+  } else {
+    backToTop.style.bottom = "-50px";
+  }
+};
+document.addEventListener("scroll", toTop);
